358e9337cb9efd6fc3765f562e85bd85ef82cb20
Guiandrade <g.andrade.1995@gmail.com>
2017-03-11 18:41:10 +0000

    Method chooseNumber() on keyStore.java

--- keyStore/src/main/java/pt/ulisboa/ist/sec/KeyStore.java
+++ keyStore/src/main/java/pt/ulisboa/ist/sec/KeyStore.java
@@ -9,19 +9,23 @@ public class KeyStore
     public static void main( String[] args ){
 
       try{
-          Scanner in = new Scanner(System.in);
-          System.out.println( "----- Welcome to the Keystore! -----" );
-          System.out.println("Please write the number of keypairs wanted : ");
-          int maxNumCertificates = in.nextInt()+1;
+          int maxNumCertificates = chooseNumber();
           for (int id=1; id<(maxNumCertificates) ; id++){
               KeyPair pair =  CertificateGenerator.generateKeyPair(id,maxNumCertificates);
               X509Certificate[] cert = CertificateGenerator.generateCertificate(pair);
               CertificateGenerator.saveToFile(cert,id,maxNumCertificates);
-
           }
           System.out.println("---- All the certificates were created ! ----");
         } catch (Exception e){
           e.printStackTrace();
         }
+    }
+
+    private static int chooseNumber(){
+      Scanner in = new Scanner(System.in);
+      System.out.println( "----- Welcome to the Keystore! -----" );
+      System.out.println("Please write the number of keypairs wanted : ");
+      int maxNumCertificates = in.nextInt()+1;
+      return maxNumCertificates;
     }
 }
